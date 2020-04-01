c69395e69111801d849701b9613ef263d8778cd1
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-04-01 14:35:58 +0700

    Hmm bentar lagi selesai nih yang css, semangat arthur

--- CSS/Praktikum/PraktikumCSS/src/AppMain.js
+++ CSS/Praktikum/PraktikumCSS/src/AppMain.js
@@ -9,33 +9,38 @@ import './dist/css/bootstrap.min.css';
 class AppMain extends Component {
     render() {
         return (
-            <div className="App-main d-xl-flex justify-content-around">
-                <section className="alert alert-secondary">
-                    <ul>
-                        <li><a href="DaftarIsi/01WidthHeight.html" target="_blank">1. Width & Height</a></li>
-                        <li><a href="DaftarIsi/02Margin.html" target="_blank">2. Margin</a></li>
-                        <li><a href="DaftarIsi/03Padding.html" target="_blank">3. Padding</a></li>
-                        <li><a href="DaftarIsi/04Border.html" target="_blank">4. Border</a></li>
-                        <li><a href="DaftarIsi/05FormatTextdanFont.html" target="_blank">5. Format Text & Font</a></li>
-                    </ul>
-                </section>
-                <section className="alert alert-secondary">
-                    <ul>
-                        <li><a href="DaftarIsi/06Background.html" target="_blank">6. Background</a></li>
-                        <li><a href="#">7. Pseudo-Class</a></li>
-                        <li><a href="#">8. Border-Radius</a></li>
-                        <li><a href="#">9. Linear-Gradient</a></li>
-                        <li><a href="#">10. Box Shadow</a></li>
-                    </ul>
-                </section>
-                <section className="alert alert-secondary">
-                    <ul>
-                        <li><a href="#">11. Nav</a></li>
-                        <li><a href="#">12. Aside</a></li>
-                        <li><a href="#">13. Card</a></li>
-                        <li><a href="#">14. Button</a></li>
-                        <li><a href="#">15. Table</a></li>
-                    </ul>
+            <div className="App-main">
+                <div className="d-xl-flex justify-content-around">
+                    <section className="alert alert-secondary">
+                        <ul>
+                            <li><a href="DaftarIsi/01WidthHeight.html" target="_blank">1. Width & Height</a></li>
+                            <li><a href="DaftarIsi/02Margin.html" target="_blank">2. Margin</a></li>
+                            <li><a href="DaftarIsi/03Padding.html" target="_blank">3. Padding</a></li>
+                            <li><a href="DaftarIsi/04Border.html" target="_blank">4. Border</a></li>
+                            <li><a href="DaftarIsi/05FormatTextdanFont.html" target="_blank">5. Format Text & Font</a></li>
+                        </ul>
+                    </section>
+                    <section className="alert alert-secondary">
+                        <ul>
+                            <li><a href="DaftarIsi/06Background.html" target="_blank">6. Background</a></li>
+                            <li><a href="DaftarIsi/07PseudoClass.html" target="_blank">7. Pseudo-Class</a></li>
+                            <li><a href="DaftarIsi/08BorderRadius.html" target="_blank">8. Border-Radius</a></li>
+                            <li><a href="DaftarIsi/09LinearGradient.html" target="_blank">9. Linear-Gradient</a></li>
+                            <li><a href="DaftarIsi/10BoxShadow.html" target="_blank">10. Box Shadow</a></li>
+                        </ul>
+                    </section>
+                    <section className="alert alert-secondary">
+                        <ul>
+                            <li><a href="#">11. Nav</a></li>
+                            <li><a href="#">12. Aside</a></li>
+                            <li><a href="#">13. Card</a></li>
+                            <li><a href="#">14. Button</a></li>
+                            <li><a href="#">15. Table</a></li>
+                        </ul>
+                    </section>
+                </div>
+                <section>
+                    <h5 id="react-version" className="text-center text-light">React version : {React.version}</h5>
                 </section>
             </div>
         );
